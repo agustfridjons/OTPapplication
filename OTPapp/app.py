56f084c97d7e3cc56847574004246dbec9d10c76
agust <agustfridjons@gmail.com>
2020-02-11 14:55:33 +0000

    Basic db functions

--- OTPapp/app.py
+++ OTPapp/app.py
@@ -1,6 +1,18 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), nullable=False)
+    password = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(50), nullable=False)
+
 
 
 @app.route('/')
