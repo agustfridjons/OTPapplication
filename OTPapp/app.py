c585d5ee7fdf349ac1f951b86c9796bc4230f7dc
agust <agustfridjons@gmail.com>
2020-03-06 15:33:31 +0000

    Commenting

--- OTPapp/app.py
+++ OTPapp/app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, url_for, request, redirect, session
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
 import re
 
 app = Flask(__name__)
@@ -10,12 +9,12 @@ app.config['SECRET_KEY'] = 'SmbmKLg9sXLBz5Z8j2KXtA'
 
 DATABASE = SQLAlchemy(app)
 
+# Creating database table
 class User(DATABASE.Model):
     id = DATABASE.Column(DATABASE.Integer, primary_key=True)
     username = DATABASE.Column(DATABASE.String(50), nullable=False)
     email = DATABASE.Column(DATABASE.String(50), nullable=False)
     password = DATABASE.Column(DATABASE.String(50), nullable=False)
-    date_created = DATABASE.Column(DATABASE.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
         return '<User %r' % self.username
@@ -28,26 +27,33 @@ def index():
 
 @app.route('/signup', methods=['POST', 'GET'])
 def signUp():
-    pas_regex = "^.*(?=.{8,})(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&+]).*$"
-    invalid_input = "[\\t\\r\\n]|(--[^\\r\\n]*)|(\\/\\*[\\w\\W]*?(?=\\*)\\*\\/)"
     db=DATABASE
+
     if request.method == 'POST':
+        # Defining regular expression strings
+        pas_regex = '^.*(?=.{8,})(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&+]).*$'
+        email_regex = '\w+[.|\w]\w+@\w+[.]\w+[.|\w+]\w+'
+
+        # receiving  content from form
         form_content = [request.form['username'], request.form['email'], request.form['password'], request.form['cpassword']]
-        for i in range(len(form_content)):
-            if not re.match(invalid_input, form_content[i]):
-                print("invalid input")
         account = User.query.filter_by(username=form_content[0]).first()
         user_exists = account != None
         print("Validating User")
-        if form_content[0] == '':
-            return redirect("/signup/error/0")
-        if user_exists:
+        # If statements checking for valid sign in
+        for i in form_content:
+            if form_content[i] == '':                   # Checking if fields are empty
+                return redirect("/signup/error/0")
+        if user_exists:                                 # Checking if user already exists
             return redirect("/signup/error/1")
-        if not re.match(pas_regex, form_content[2]):
+        if not re.match(email_regex, form_content[1]):  # Checking if eamil follows the right format
+            return redirect("/signup/error/4")
+        if not re.match(pas_regex, form_content[2]):    # Checking if password follows the right format
             return redirect("/signup/error/2")
-        if not (form_content[2] == form_content[3]):
+        if not (form_content[2] == form_content[3]):    # Checking if 'password' and 'confirmed password' match
             return redirect("/signup/error/3")
         print("Making user")
+
+        # Adds the new user to the DB
         new_user = User(username=form_content[0], email=form_content[1], password=form_content[2])
         try:
             print("Adding to db")
@@ -64,41 +70,44 @@ def signUp():
 @app.route('/signin', methods=['GET', 'POST'])
 def signIn():
     if request.method == 'POST':
+        # Fetching input fields from form
         req = request.form
         username = req.get("username")
         password = req.get("password")
         account = User.query.filter_by(username=username).first()
         user_exists = account != None
-        if user_exists:
-            #OPT toodoo
-            if account.password == password:
+
+        # If username exists and password is right send OTP
+        if user_exists & account.password == password:
+            if True: #TODO email OPT
                 session['username'] = account.username
                 return redirect(url_for('index'))
-            else:
-                return "Invalid username or password"
         else:
             return "Invalid username or password"
     else:
         return render_template('signin.html')
 
 
+# Handling Error messages
 @app.route('/signup/error/<id>')
 def signup_error(id):
     if id == '0':
-        message = 'Username cannot be empty'
+        message = 'Fields cannot be empty'
     if id == '1':
-        message = 'User already exists'
+        message = 'Invalid username'
+    if id == '4':
+        message = 'Invalid email address'
     if id == '2':
         message = 'Password doesn\'t fulfill requirements'
     if id == '3':
         message = 'Passwords have to match'
     return render_template('signup.html', message=message)
 
-
+# Takes User out of session
 @app.route('/signout')
 def signOut():
     session.pop('username', None)
-    return redirect('/')
+    return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
