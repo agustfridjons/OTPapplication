f0b499244d7d05ec31dbb81661604e2ad44a0e4b
agust <agustfridjons@gmail.com>
2020-05-20 21:40:35 +0000

    Confirm email finished

--- OTPapp/app.py
+++ OTPapp/app.py
@@ -1,20 +1,30 @@
+from datetime import datetime
 from flask import Flask, render_template, url_for, request, redirect, session
 from flask_sqlalchemy import SQLAlchemy
+from flask_mail import Mail, Message
+from itsdangerous import URLSafeTimedSerializer, SignatureExpired
 import re
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 app.config['SECRET_KEY'] = 'SmbmKLg9sXLBz5Z8j2KXtA'
+app.config.from_pyfile('config.cfg')
 
-DATABASE = SQLAlchemy(app)
+URLSS = URLSafeTimedSerializer(app.config['SECRET_KEY'])
+
+mail = Mail(app)
+
+db = SQLAlchemy(app)
 
 # Creating database table
-class User(DATABASE.Model):
-    id = DATABASE.Column(DATABASE.Integer, primary_key=True)
-    username = DATABASE.Column(DATABASE.String(50), nullable=False)
-    email = DATABASE.Column(DATABASE.String(50), nullable=False)
-    password = DATABASE.Column(DATABASE.String(50), nullable=False)
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(50), nullable=False)
+    password = db.Column(db.String(50), nullable=False)
+    email_confirmed = db.Column(db.Boolean, nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.now)
 
     def __repr__(self):
         return '<User %r' % self.username
@@ -25,9 +35,45 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/expired', methods=['POST', 'GET'])
+def emailExpired():
+    if request.method == 'POST':
+        email = request.form['email']
+        account = User.query.filter_by(email=email).first()
+        user_exists = account != None
+        if user_exists:
+            sendConfirmationEmail(email)
+            return render_template('expired.html', message="Email sent!")
+    else:
+        return render_template('expired.html', message="")
+
+
+#User has an hour to respond
+@app.route('/confirm_email/<token>')
+def confirmEmail(token):
+    try:
+        email = URLSS.loads(token, salt='rMb2LL4smLvY5Z8x2KSbP', max_age=3600)
+    except SignatureExpired:
+        return render_template('expired.html', message="")
+    account = User.query.filter_by(email=email).first()
+    user_exists = account != None
+    if user_exists:
+        account.email_confirmed = True
+        db.session.commit()
+        return render_template('confirmemail.html')
+    else:
+        return render_template('expired.html', message="Error confirming email")
+
+def sendConfirmationEmail(email):
+    token = URLSS.dumps(email, salt='rMb2LL4smLvY5Z8x2KSbP')
+    msg = Message('Confirm Email', sender='gustividdi@gmail.com', recipients=[email])
+    link = url_for('confirmEmail', token=token, _external=True)
+    msg.body = 'Your confirmation link is {}'.format(link)
+    mail.send(msg)
+
 @app.route('/signup', methods=['POST', 'GET'])
 def signUp():
-    db=DATABASE
+    database = db
 
     if request.method == 'POST':
         # Defining regular expression strings
@@ -37,28 +83,33 @@ def signUp():
         # receiving  content from form
         form_content = [request.form['username'], request.form['email'], request.form['password'], request.form['cpassword']]
         account = User.query.filter_by(username=form_content[0]).first()
+        account_email = User.query.filter_by(email=form_content[1]).first()
         user_exists = account != None
+        email_exists = account_email != None
         print("Validating User")
         # If statements checking for valid sign in
         for i in form_content:
-            if form_content[i] == '':                   # Checking if fields are empty
+            if i == '':                   # Checking if fields are empty
                 return redirect("/signup/error/0")
         if user_exists:                                 # Checking if user already exists
             return redirect("/signup/error/1")
-        if not re.match(email_regex, form_content[1]):  # Checking if eamil follows the right format
+        if (not re.match(email_regex, form_content[1])) | email_exists:  # Checking if email follows the right format
             return redirect("/signup/error/4")
         if not re.match(pas_regex, form_content[2]):    # Checking if password follows the right format
             return redirect("/signup/error/2")
         if not (form_content[2] == form_content[3]):    # Checking if 'password' and 'confirmed password' match
             return redirect("/signup/error/3")
-        print("Making user")
 
+
+        print("Making user")
         # Adds the new user to the DB
-        new_user = User(username=form_content[0], email=form_content[1], password=form_content[2])
+        new_user = User(username=form_content[0], email=form_content[1], password=form_content[2], email_confirmed=False)
+        print("Sending confirm email")
+        sendConfirmationEmail(form_content[1])
         try:
             print("Adding to db")
-            db.session.add(new_user)
-            db.session.commit()
+            database.session.add(new_user)
+            database.session.commit()
             print("User added")
             return redirect(url_for('index'))
         except:
@@ -78,12 +129,14 @@ def signIn():
         user_exists = account != None
 
         # If username exists and password is right send OTP
-        if user_exists & account.password == password:
-            if True: #TODO email OPT
+        if user_exists & (account.password == password):
+            if account.email_confirmed: #TODO email OPT
                 session['username'] = account.username
                 return redirect(url_for('index'))
+            else:
+                return 'Invalid username or password, <a href="/expired"> email might not have been confirmed</a>'
         else:
-            return "Invalid username or password"
+            return 'Invalid username or password, <a href="/expired"> email might not have been confirmed</a>'
     else:
         return render_template('signin.html')
 
@@ -108,7 +161,6 @@ def signup_error(id):
 def signOut():
     session.pop('username', None)
     return redirect(url_for('index'))
-
 
 if __name__ == '__main__':
     app.run(debug=True)
